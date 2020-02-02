849f2a1a4010534d311b4518ad784a0196d549c3
Jean Barascu <narcis86@gmail.com>
2020-02-02 16:54:35 +0200

    Scaffold and mobile styles for five days forecast component

--- src/components/FiveDaysForecast/index.js
+++ src/components/FiveDaysForecast/index.js
@@ -2,7 +2,62 @@ import React from "react";
 import "./FiveDaysForecast.css";
 
 const FiveDaysForecast = () => {
-  return <h6>Five Days Forecast</h6>;
+  return (
+    <div className="WeatherApp__fiveDaysForecastContent">
+      <ul className="WeatherApp__fiveDaysForecastItems">
+        <li className="WeatherApp__fiveDaysForecastItem">
+          <p className="WeatherApp__fiveDaysForecastDay">Monday</p>
+          <img
+            className="WeatherApp__fiveDaysForecastItemIcon"
+            src="http://openweathermap.org/img/wn/11d@2x.png"
+            alt="Wheater Icon"
+          />
+          <p className="WeatherApp__fiveDaysForecastItemMinTemp">18&#8451;</p>
+          <p className="WeatherApp__fiveDaysForecastItemMaxTemp">28&#8451;</p>
+        </li>
+        <li className="WeatherApp__fiveDaysForecastItem">
+          <p className="WeatherApp__fiveDaysForecastDay">Tuesday</p>
+          <img
+            className="WeatherApp__fiveDaysForecastItemIcon"
+            src="http://openweathermap.org/img/wn/11d@2x.png"
+            alt="Wheater Icon"
+          />
+          <p className="WeatherApp__fiveDaysForecastItemMinTemp">18&#8451;</p>
+          <p className="WeatherApp__fiveDaysForecastItemMaxTemp">28&#8451;</p>
+        </li>
+        <li className="WeatherApp__fiveDaysForecastItem">
+          <p className="WeatherApp__fiveDaysForecastDay">Wednesday</p>
+          <img
+            className="WeatherApp__fiveDaysForecastItemIcon"
+            src="http://openweathermap.org/img/wn/11d@2x.png"
+            alt="Wheater Icon"
+          />
+          <p className="WeatherApp__fiveDaysForecastItemMinTemp">18&#8451;</p>
+          <p className="WeatherApp__fiveDaysForecastItemMaxTemp">28&#8451;</p>
+        </li>
+        <li className="WeatherApp__fiveDaysForecastItem">
+          <p className="WeatherApp__fiveDaysForecastDay">Thursday</p>
+          <img
+            className="WeatherApp__fiveDaysForecastItemIcon"
+            src="http://openweathermap.org/img/wn/11d@2x.png"
+            alt="Wheater Icon"
+          />
+          <p className="WeatherApp__fiveDaysForecastItemMinTemp">18&#8451;</p>
+          <p className="WeatherApp__fiveDaysForecastItemMaxTemp">28&#8451;</p>
+        </li>
+        <li className="WeatherApp__fiveDaysForecastItem">
+          <p className="WeatherApp__fiveDaysForecastDay">Friday</p>
+          <img
+            className="WeatherApp__fiveDaysForecastItemIcon"
+            src="http://openweathermap.org/img/wn/11d@2x.png"
+            alt="Wheater Icon"
+          />
+          <p className="WeatherApp__fiveDaysForecastItemMinTemp">18&#8451;</p>
+          <p className="WeatherApp__fiveDaysForecastItemMaxTemp">28&#8451;</p>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default FiveDaysForecast;
