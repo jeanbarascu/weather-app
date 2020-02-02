8913526ec2b28d498b51d36dfcdb631c21f63368
Jean Barascu <narcis86@gmail.com>
2020-02-02 16:26:32 +0200

    Mobile css styles for one day forecast component

--- src/components/OneDayForecast/index.js
+++ src/components/OneDayForecast/index.js
@@ -2,7 +2,24 @@ import React from "react";
 import "./OneDayForecast.css";
 
 const OneDayForecast = () => {
-  return <h3>One Day Forecast</h3>;
+  return (
+    <>
+      <img
+        className="WeatherApp__oneDayForecastImg"
+        src="http://openweathermap.org/img/wn/11d@2x.png"
+        alt="Wheater Icon"
+      />
+      <div className="WeatherApp__oneDayForecastContent">
+        <h1 className="WeatherApp__oneDayForecastTitle">Thunderstorm</h1>
+        <span className="WeatherApp__oneDayForecastCurrentTemp">
+          16 &#8451;
+        </span>
+        <span className="WeatherApp__oneDayForecastMinMaxTemp">
+          min. 8 &#8451; / max. 22 &#8451;
+        </span>
+      </div>
+    </>
+  );
 };
 
 export default OneDayForecast;
